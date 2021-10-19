58f551d55b03270dfb5043d74f415cb1cd096638
bgyoon <gye04056@gmail.com>
2021-10-20 00:01:51 +0900

    Techniques for dealing with React

---
+++ 10todo/src/App.js
@@ -0,0 +1,56 @@
+import React, { useCallback, useRef, useState } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '리액트 기초', checked: true },
+    { id: 2, text: '컴포넌트 스타일링', checked: true },
+    { id: 3, text: '일정관리앱', checked: false },
+  ]);
+  // 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          // 참일 때 새로운 객체 생성
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
+
+export default App;
